8dd9a24d3c97f28b9d02fa8abf1f89db0fd6ef1f
WD40Drinker <Mitchtvu@hotmail.com>
2025-04-18 22:35:01 -0500

    Update float_side.ino

--- float_side/float_side.ino
+++ float_side/float_side.ino
@@ -4,15 +4,27 @@
 
 SoftwareSerial Bluetooth(3,2);
 
-// declare sensor variables
+
+struct Data {
+long time = -1;
+float temp = -1;
+float depth = -1;
+float pressure = -1;
+float altitude = -1;
+};
+
+// declare temporary sensor variables
 String readDepth = "";
-long time;
 int toDepth;
+long time;
 float temp;
 float depth;
 float pressure;
 float altitude;
-String incomingMessage = "";
+Data collectedData[500]; // we will save our collected data into this array (we probably only need 500 points anyway)
+int entry = 0; // location of the entry in the collectedData array
+
+String incomingMessage = ""; // buffer for any data from mission station
 MS5837 sensor;
 // declare H-driver variables
 int enA = 11;
@@ -21,10 +33,10 @@ int in2 = 9;
 
 // declare other variables
 int step = 0; // This will be our actuator position tracking variables.  step is our counter
-int stepMax = 10;  // and stepMax will be our limit
+int stepMax = 10;  // and stepMax will be our limit (if it exceeds it, the float will return)
 int increment = 5000; // this will be time our functions actuateForware() and actualteBackward() will run for
 
-String message = String("Dive completed successfully");
+bool reached = true; // determines the message printed after a dive is completed
 
 void setup() {
 // put your setup code here, to run once:
@@ -50,11 +62,20 @@ void setup() {
 
 void loop() {
   if(Bluetooth.available()){
-    Serial.println("Bluetooth Connected");
-    Bluetooth.write("can you hear me?");
+    Serial.println("Bluetooth Connected:");
+    Bluetooth.write("Team 2: Help, i am unda da water");
+    time = millis();
+    pressure = sensor.pressure();
+    depth = sensor.depth();
+    Bluetooth.write(time);
+    Bluetooth.write("pressure: ");
+    Bluetooth.write(pressure);
+    Bluetooth.write("depth: ");
+    Bluetooth.write(depth);
+
     Bluetooth.write("enter \"101\" to verify and start dive");
 
-    bool verification = true;
+    bool verification = true; // while loop waits for user verification from mission station
     while(verification){
       //timeNow = millis();
       while (Bluetooth.available()) {
@@ -64,18 +85,18 @@ void loop() {
       if (c == '\n') {
         incomingMessage.trim();  // Remove whitespace/newline
         Serial.println("Received: " + incomingMessage);
-        if (incomingMessage == "101") {
-          Serial.println("Bluetooth Signal Validated");
-          Bluetooth.println("000");
-          communicateData();
-          verification = false;  // Exit loop
-        }
-        incomingMessage = "";  // Reset message buffer
+          if (incomingMessage == "101") {
+            Serial.println("Bluetooth Signal Validated");
+            Bluetooth.write("Bluetooth Signal Validated");
+            //communicateData();
+            verification = false;  // Exit loop
+          }
+          incomingMessage = "";  // Reset message buffer
         }
       }
     }
 
-    bool waitForInput = true;
+    bool waitForInput = true; // while loop waits for input from mission station
     while(waitForInput){
       //timeNow = millis();
       while (Bluetooth.available()) {
@@ -96,14 +117,13 @@ void loop() {
     }
 
     Bluetooth.println("Starting Dive: ");
-	  message = "Dive completed successfully";
     //do stuff to sink the float
     readDepth = String(Bluetooth.read()); //I don't remember do I have to typecast this? 
     toDepth = readDepth.toInt();
     Bluetooth.write("float is going to" + toDepth);
     Serial.println("Starting Dive");
 
-    while(toDepth > depth){
+    while(toDepth > depth){ // IMPORTANT: CHECK THIS: WILL TYPES BE A PROBLEM?
       //increment down with the actuator
       delay(1000); // delay to upload new instructions  idk if I really need this 
       actuateForward();
@@ -113,7 +133,7 @@ void loop() {
       communicateData();
       depth = sensor.depth();
 	    if(step >= stepMax){
-		    message = "Cannot go to depth: " + toDepth;
+		    reached = false;
 		    break;
 	    }
     }
@@ -127,13 +147,21 @@ void loop() {
       actuateBackward();
       delay(increment);
       coastStop();
+      communicateData();
       delay(1000);
     }
     step = 0; // never hurts to be redundant
 
     //send the saved info
-    Bluetooth.write(&message); // send info back to the other arduino 
+    if(reached== true){
+      Bluetooth.write("Dive completed successfully");
+    }
+    else{
+      Bluetooth.write("Could not reach depth " + toDepth);
+    }
+
     communicateData();
+    reached = true; // dive is done, reset reached variable
   }
 }
 
@@ -144,13 +172,22 @@ void communicateData() {
   altitude = sensor.altitude();
   time = millis();
   String Stringtime = String(time);
+  //Bluetooth.write("Time: ");
   Bluetooth.write(time);
+  Bluetooth.write("Temp: ");
   Bluetooth.write(temp);
+  Bluetooth.write("Depth: ");
   Bluetooth.write(depth);
+  Bluetooth.write("Pressure: ");
   Bluetooth.write(pressure);
+  Bluetooth.write("Altitiude: ");
   Bluetooth.write(altitude);
-
-
+  collectedData[entry].time = time;
+  collectedData[entry].temp = temp;
+  collectedData[entry].depth = depth;
+  collectedData[entry].pressure = pressure;
+  collectedData[entry].altitude = altitude; // lol you could totally clean this up :)
+  entry++;
 }
 
 // declare H-driver functions
@@ -179,13 +216,12 @@ void brakeStop() {
 }
 
 void forceMin() {
+  Serial.println("starting min routine");
   for(int i = 0; i < stepMax; i++){
-
     actuateBackward();
     delay(increment);
     coastStop();
   }
 }
-
 
 
