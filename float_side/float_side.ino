1d3952ece3eeed1498697527f44ff95f8c2caea6
WD40Drinker <Mitchtvu@hotmail.com>
2025-04-23 18:02:58 -0500

    updated H-driver instructions

--- float_side/float_side.ino
+++ float_side/float_side.ino
@@ -4,6 +4,7 @@
 
 SoftwareSerial Bluetooth(3,2);
 
+//when adjusting depth, make more boyant for incremement time and then make neutrally boyant
 
 struct Data {
 long time = -1;
@@ -21,22 +22,21 @@ float temp;
 float depth;
 float pressure;
 float altitude;
-Data collectedData[500]; // we will save our collected data into this array (we probably only need 500 points anyway)
+Data collectedData[50]; // we will save our collected data into this array (we probably only need 500 points anyway)
 int entry = 0; // location of the entry in the collectedData array
 
 String incomingMessage = ""; // buffer for any data from mission station
 MS5837 sensor;
 // declare H-driver variables
-int enA = 11;
-int in1 = 10;
-int in2 = 9;
-
-// declare other variables
-int step = 0; // This will be our actuator position tracking variables.  step is our counter
-int stepMax = 10;  // and stepMax will be our limit (if it exceeds it, the float will return)
-int increment = 5000; // this will be time our functions actuateForware() and actualteBackward() will run for
-
-bool reached = true; // determines the message printed after a dive is completed
+const int IN3 = 11; // Extend
+const int IN4 = 10; // Retract
+const int ENB = 9;  // Motor speed
+int actuateFrom = 0;
+int actuateTo = 0;
+bool isMoving = false;
+unsigned long movementStart = 0;
+unsigned long movementDuration = 0;
+const int neutral = 50;
 
 void setup() {
 // put your setup code here, to run once:
@@ -45,19 +45,19 @@ void setup() {
   Wire.begin();
   sensor.setModel(MS5837::MS5837_30BA);
   sensor.setFluidDensity(997);
+  Bluetooth.begin(38400);
 
-  Bluetooth.begin(38400); //HC-05 default speed in AT command mode
+  //setup H-driver pins
+  pinMode(IN3, OUTPUT);
+  pinMode(IN4, OUTPUT);
+  pinMode(ENB, OUTPUT);
+  digitalWrite(IN4,HIGH);
+  analogWrite(ENB,255);
+  delay(8000);
+  stopMotor();
 
-  pinMode(enA, OUTPUT);
-  pinMode(in1, OUTPUT);
-  pinMode(in2, OUTPUT);
-  // Turn off motors - Initial state
-  digitalWrite(in1, LOW);
-  digitalWrite(in2, LOW);
-
-  // min our the H-driver
-  forceMin();
-  step = 0; // never hurts to be redundant
+  // mid our the H-driver
+  forceMid(); // we need a function to make the H-driver neutral
 }
 
 void loop() {
@@ -72,19 +72,18 @@ void loop() {
     Bluetooth.write(pressure);
     Bluetooth.write("depth: ");
     Bluetooth.write(depth);
-
     Bluetooth.write("enter \"101\" to verify and start dive");
 
     bool verification = true; // while loop waits for user verification from mission station
     while(verification){
       //timeNow = millis();
       while (Bluetooth.available()) {
-      char c = Bluetooth.read();
-      incomingMessage += c;
-      // If we see a newline or carriage return, process the message
-      if (c == '\n') {
-        incomingMessage.trim();  // Remove whitespace/newline
-        Serial.println("Received: " + incomingMessage);
+        char c = Bluetooth.read();
+        incomingMessage += c;
+        // If we see a newline or carriage return, process the message
+        if (c == '\n') {
+          incomingMessage.trim();  // Remove whitespace/newline
+          Serial.println("Received: " + incomingMessage);
           if (incomingMessage == "101") {
             Serial.println("Bluetooth Signal Validated");
             Bluetooth.write("Bluetooth Signal Validated");
@@ -98,13 +97,12 @@ void loop() {
 
     bool waitForInput = true; // while loop waits for input from mission station
     while(waitForInput){
-      //timeNow = millis();
       while (Bluetooth.available()) {
-      char c = Bluetooth.read();
-      if(isDigit(c)){
-        incomingMessage += c;
-      }
-      // If we see a newline or carriage return, process the message
+        char c = Bluetooth.read();
+        if(isDigit(c)){
+          incomingMessage += c;
+        }
+        // If we see a newline or carriage return, process the message
         if (c == '\n') {
           incomingMessage.trim();  // Remove whitespace/newline
           Serial.println("Received: " + incomingMessage);
@@ -116,112 +114,131 @@ void loop() {
       }
     }
 
-    Bluetooth.println("Starting Dive: ");
+    Bluetooth.println("Starting Dive");
     //do stuff to sink the float
-    readDepth = String(Bluetooth.read()); //I don't remember do I have to typecast this? 
-    toDepth = readDepth.toInt();
     Bluetooth.write("float is going to" + toDepth);
-    Serial.println("Starting Dive");
+    Serial.println("Starting Dive (for real this time)");
+
+    startMovement(70); // make the float sink by making it heavier
 
     while(toDepth > depth){ // IMPORTANT: CHECK THIS: WILL TYPES BE A PROBLEM?
-      //increment down with the actuator
-      delay(1000); // delay to upload new instructions  idk if I really need this 
-      actuateForward();
-      delay(increment);
-      coastStop();
-      delay(1000); //give the sensor time to update its depth 
-      communicateData();
-      depth = sensor.depth();
-	    if(step >= stepMax){
-		    reached = false;
-		    break;
-	    }
+      delay(5000); //give the sensor time to update its depth 
+      saveData();
+      //depth = sensor.depth();
     }
     //idk how to program adjustments when the float overshoots
     //This is the depth. Fuck you :)
 
-    delay(5000); // lets pause just because
-    //empty the valve (min out the H-driver)
+    //delay(5000); // lets pause just because
+
+    //make the float surface 
     Bluetooth.write("float is surfacing");
-    for(int i = 0; i < step; i++){
-      actuateBackward();
-      delay(increment);
-      coastStop();
-      communicateData();
-      delay(1000);
+    startMovement(neutral - 20);
+
+    while(depth > 50){
+      delay(100);
+      depth = sensor.depth();
     }
-    step = 0; // never hurts to be redundant
 
     //send the saved info
-    if(reached== true){
-      Bluetooth.write("Dive completed successfully");
-    }
-    else{
-      Bluetooth.write("Could not reach depth " + toDepth);
-    }
+    Bluetooth.write("Dive completed successfully");
 
-    communicateData();
-    reached = true; // dive is done, reset reached variable
+    startMovement(neutral + 20);
+    dataDump();
+
+    forceMid();
   }
 }
 
-void communicateData() {
+void saveData() {
   temp = sensor.temperature();
   depth = sensor.depth();
   pressure = sensor.pressure();
   altitude = sensor.altitude();
   time = millis();
   String Stringtime = String(time);
-  //Bluetooth.write("Time: ");
-  Bluetooth.write(time);
-  Bluetooth.write("Temp: ");
-  Bluetooth.write(temp);
-  Bluetooth.write("Depth: ");
-  Bluetooth.write(depth);
-  Bluetooth.write("Pressure: ");
-  Bluetooth.write(pressure);
-  Bluetooth.write("Altitiude: ");
-  Bluetooth.write(altitude);
   collectedData[entry].time = time;
   collectedData[entry].temp = temp;
   collectedData[entry].depth = depth;
   collectedData[entry].pressure = pressure;
   collectedData[entry].altitude = altitude; // lol you could totally clean this up :)
+  //print to serial for testing purposes 
+  Serial.println(collectedData[entry].time);
+  Serial.println("Temp: ");
+  Serial.println(collectedData[entry].temp);
+  Serial.println("Depth: ");
+  Serial.println(collectedData[entry].depth);
+  Serial.println("Pressure: ");
+  Serial.println(collectedData[entry].pressure);
+  Serial.println("Altitiude: ");
+  Serial.println(collectedData[entry].altitude);
   entry++;
 }
 
-// declare H-driver functions
-  void actuateForward() {
-  analogWrite(enA, 255);
-  digitalWrite(in1, HIGH);
-  digitalWrite(in2, LOW);
-  step ++;
-}
-
-void actuateBackward() {
-  analogWrite(enA, 255);
-  digitalWrite(in1, LOW);
-  digitalWrite(in2, HIGH);
-  step --;
-}
-
-void coastStop() {
-  digitalWrite(in1, LOW);
-  digitalWrite(in2, LOW);
-}
-
-void brakeStop() {
-  digitalWrite(in1, HIGH);
-  digitalWrite(in2, HIGH);
-}
-
-void forceMin() {
-  Serial.println("starting min routine");
-  for(int i = 0; i < stepMax; i++){
-    actuateBackward();
-    delay(increment);
-    coastStop();
+void dataDump(){
+  Bluetooth.write("time | temp | depth | pressure | altitiude");
+  for(int i = 0; i < 50; i++){
+    if(collectedData[i].time != -1){
+      Bluetooth.write(collectedData[i].time);
+      Bluetooth.write(collectedData[i].temp);
+      Bluetooth.write(collectedData[i].depth);
+      Bluetooth.write(collectedData[i].pressure);
+      Bluetooth.write(collectedData[i].altitude);
+      Bluetooth.write("\n");
+    }
   }
+
+}
+
+void startMovement(int i) {
+	actuateTo = i;
+  int diff = actuateTo - actuateFrom;
+
+  Serial.print("Moving from ");
+  Serial.print(actuateFrom);
+  Serial.print("% to ");
+  Serial.print(actuateTo);
+  Serial.println("%");
+
+  movementStart = millis();
+  movementDuration = abs(diff) * 70;  // Tune this: ms per % change
+
+  if (diff > 0) {
+    // Extend
+    digitalWrite(IN3, HIGH);
+    digitalWrite(IN4, LOW);
+  } else {
+    // Retract
+    digitalWrite(IN3, LOW);
+    digitalWrite(IN4, HIGH);
+  }
+
+  analogWrite(ENB, 200); // PWM speed
+  isMoving = true;
+
+	// put while loop here 
+	// when the stop happens/ escape while loop
+    while (isMoving) {
+    if (millis() - movementStart >= movementDuration) {
+      stopMotor();
+      actuateFrom = actuateTo; // ✅ ONLY update when move completes
+      Serial.print("Reached ");
+      Serial.print(actuateFrom);
+      Serial.println("%");
+    }
+  }
+}
+
+void stopMotor() {
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, LOW);
+  analogWrite(ENB, 0);
+  isMoving = false;
+}
+
+void forceMid() {
+  Serial.println("starting mid routine");
+  startMovement(neutral);
 }
 
 
